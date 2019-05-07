60ca1617da2516761215e12c22a36bb1408140d0
ZWkang <907747874@qq.com>
2019-05-07 12:23:58 +0800

    ADDED: add ctressa.ts file

---
+++ ctressa.ts
@@ -0,0 +1,13 @@
+declare namespace ctressa {
+    type Inspect = (val: any) => string
+    enum showDiff { undefined, false, true}
+    interface AssertionErrorOptions {
+        message?: String,
+        name?: String,
+        actual?: any,
+        expected?: any,
+        showDiff: showDiff,
+        stackStartFn?: Function
+    }
+    
+}
